f9ff921de635a823245bb494b63794398ec897c8
Scout064 <78973583+Scout064@users.noreply.github.com>
2021-02-18 16:45:08 +0100

    Update prep_cert.sh

--- prep_cert.sh
+++ prep_cert.sh
@@ -7,7 +7,8 @@ sudo rm /Path/to/cert/cert.p12
 sudo cp /etc/letsencrypt/live/fqdn.domain.com/cert.xxx /Path/to/cert/
 #Convert, Password-Protect and copy over Certificate (if needed)
 #sudo openssl pkcs12 -export -passout pass:'Password' -out /Path/to/cert/cert.p12 -inkey /etc/letsencrypt/live/fqdn.domain.com/fullchain.pem -in /etc/letsencrypt/live/fqdn.domain.com/cert.pem
-#set Permissions to cert folder 
+#set Permissions to cert folder
+#propably needs changing if rpi is public :P
 sudo chmod 777 -R /Path/to/cert
 sudo chown -R username:usergroup /Path/to/cert/*
 #execute send_cert.sh
