52b2fb85c2356a1653844df0025d6a4b5dbca0f4
Scout064 <78973583+Scout064@users.noreply.github.com>
2021-02-18 16:36:39 +0100

    Add files via upload

---
+++ prep_cert.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+#run renew hooks for Certbot
+sudo certbot renew
+#remove existing Certificates in export Location
+sudo rm /Path/to/cert/cert.p12
+#copy over needed Certificate from LE Live folder
+sudo cp /etc/letsencrypt/live/fqdn.domain.com/cert.xxx /Path/to/cert/
+#Convert, Password-Protect and copy over Certificate (if needed)
+#sudo openssl pkcs12 -export -passout pass:'Password' -out /Path/to/cert/cert.p12 -inkey /etc/letsencrypt/live/fqdn.domain.com/fullchain.pem -in /etc/letsencrypt/live/fqdn.domain.com/cert.pem
+#set Permissions to cert folder 
+sudo chmod 777 -R /Path/to/cert
+sudo chown -R username:usergroup /Path/to/cert/*
+#execute send_cert.sh
+/Path/to/send_cert.sh
---
+++ send_cert.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+  # send mails via external-mail-server that supports smpt-authentication via password
+  # behind -xu needs to be the username that is used to smpt-login to the server, in this case it is equal to sender's mail address, but does not necessarily need to be the same!
+
+sendemail -o tls=yes -s yourmx.server.com -f recipient@domain.com -t sender@domain.com -u "E-mail subject" -xu smtpuser@domain.com -xp "YourPassword" -m "E-Mail body" -a /Path/to/cert/cert.p12
+
